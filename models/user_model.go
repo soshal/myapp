db687a235a1073d59e20645450214169d19d6697
soshal <111147773+soshal@users.noreply.github.com>
2024-06-21 14:54:22 +0530

    Update user_model.go

--- models/user_model.go
+++ models/user_model.go
@@ -1,40 +1,41 @@
 package models
 
 import (
-    "assignment_project/config"
-    "database/sql"
-    "errors"
+	"assignment_project/config"
+	"errors"
 )
 
 var (
-    ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
+
+//roles
 
 type Role string
 
 const (
-    RoleSales         Role = "Sales"
-    RoleAccountant    Role = "Accountant"
-    RoleHR            Role = "HR"
-    RoleAdministrator Role = "Administrator"
+	RoleSales         Role = "Sales"
+	RoleAccountant    Role = "Accountant"
+	RoleHR            Role = "HR"
+	RoleAdministrator Role = "Administrator"
 )
 
 type UserCredentials struct {
-    Username string `json:"username"`
-    Password string `json:"password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 type User struct {
-    ID       int    `json:"id"`
-    Username string `json:"username"`
-    Password string `json:"password"`
-    Role     Role   `json:"role"`
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Role     Role   `json:"role"`
 }
 
 func GetUserByUsername(username string) (User, error) {
-    var user User
-    query := "SELECT id, username, password, role FROM users WHERE username=$1"
-    row := config.DB.QueryRow(query, username)
-    err := row.Scan(&user.ID, &user.Username, &user.Password, &user.Role)
-    return user, err
+	var user User
+	query := "SELECT id, username, password, role FROM users WHERE username=$1"
+	row := config.DB.QueryRow(query, username)
+	err := row.Scan(&user.ID, &user.Username, &user.Password, &user.Role)
+	return user, err
 }
